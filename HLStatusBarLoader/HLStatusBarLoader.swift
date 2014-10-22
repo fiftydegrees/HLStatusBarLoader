9c416a793224018d38e2798557351ed7b39ea9db
Hervé DROIT <herve.droit@3ie.fr>
2014-10-22 17:32:36 +0200

    Added enhancement for issue #2 - You can now properly call show() or hide() methods during your view controller instance lifecycle to manage the loader.

--- HLStatusBarLoader/HLStatusBarLoader.swift
+++ HLStatusBarLoader/HLStatusBarLoader.swift
@@ -9,66 +9,84 @@
 import Foundation
 import UIKit
 
-private var xoForegroundColorAssociationKey: UInt8    = 0
-private var xoBackgroundColorAssociationKey: UInt8    = 1
+private var xoForegroundColorAssociationKey: UInt8 = 0
+private var xoBackgroundColorAssociationKey: UInt8 = 1
 
-private var xoTimerAssociationKeyy: NSTimer    = NSTimer()
+private var xoTimerAssociationKeyy: NSTimer        = NSTimer()
+
+private let _LoaderHUDSharedInstance = LoaderHUD(frame: CGRectMake(0.0, -20.0,
+                                                                   CGRectGetWidth(UIScreen.mainScreen().bounds), 2.0))
+
+class LoaderHUD: UIView {
+    
+    var factoryTimer: NSTimer?
+    
+    var foregroundColor: UIColor?
+    
+    class var sharedInstance: LoaderHUD {
+        return _LoaderHUDSharedInstance
+    }
+    
+    func startLoadingAnimation(animated: Bool) -> Void {
+        
+        stopLoadingAnimation(false)
+        
+        UIView.animateWithDuration((animated ? 0.2 : 0.0), animations: { () -> Void in
+            LoaderHUD.sharedInstance.alpha = 1.0
+        })
+        
+        addSwipingView()
+        factoryTimer = NSTimer.scheduledTimerWithTimeInterval(0.7, target: self, selector: "addSwipingView", userInfo: nil, repeats: true)
+    }
+    
+    func stopLoadingAnimation(animated: Bool) -> Void {
+        
+        factoryTimer?.invalidate()
+        factoryTimer = nil
+        
+        UIView.animateWithDuration((animated ? 0.2 : 0.0), animations: { () -> Void in
+            LoaderHUD.sharedInstance.alpha = 0.0
+        })
+    }
+    
+    func addSwipingView() {
+        
+        var loadingView: UIView         = UIView(frame: CGRectMake(CGRectGetWidth(LoaderHUD.sharedInstance.bounds), 0.0,
+                                                           CGRectGetWidth(LoaderHUD.sharedInstance.bounds) / 4.0, CGRectGetHeight(LoaderHUD.sharedInstance.bounds)))
+        loadingView.backgroundColor     = (foregroundColor != nil ? foregroundColor : UIColor.blackColor())
+        
+        LoaderHUD.sharedInstance.addSubview(loadingView)
+
+        var animation: CABasicAnimation = CABasicAnimation()
+        animation.duration              = 2.0
+        animation.keyPath               = "position.x";
+        animation.fromValue             = CGRectGetWidth(LoaderHUD.sharedInstance.bounds) + CGRectGetWidth(loadingView.frame)
+        animation.toValue               = -CGRectGetWidth(LoaderHUD.sharedInstance.bounds)
+        animation.timingFunction        = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        
+        loadingView.layer.addAnimation(animation, forKey: "basic")
+    }
+}
 
 extension UIViewController {
     
-    var loaderForegroundColor: UIColor? {
+    var statusBarHUDForegroundColor: UIColor? {
         
-        get {
-            
-            var color: UIColor? = objc_getAssociatedObject(self, &xoForegroundColorAssociationKey) as UIColor?
-            
-            if color != nil {
-                return color
-            }
-            else {
-                return UIColor.blackColor()
-            }
+        get { return LoaderHUD.sharedInstance.foregroundColor }
+        set (newColor) { LoaderHUD.sharedInstance.foregroundColor = newColor }
+    }
+    
+    func showStatusBarHUD() -> Void {
+        
+        if LoaderHUD.sharedInstance.superview == nil {
+            self.navigationController?.navigationBar.addSubview(LoaderHUD.sharedInstance)
         }
         
-        set (newColor) { objc_setAssociatedObject(self, &xoForegroundColorAssociationKey, newColor, UInt(OBJC_ASSOCIATION_RETAIN)) }
+        LoaderHUD.sharedInstance.startLoadingAnimation(true)
     }
     
-    private var factoryTimer: NSTimer? {
-        get { return objc_getAssociatedObject(self, &xoTimerAssociationKeyy) as? NSTimer }
-        set (newTimer) {
-            objc_setAssociatedObject(self, &xoTimerAssociationKeyy, newTimer, UInt(OBJC_ASSOCIATION_RETAIN)) }
-    }
-    
-    func startLoading() -> Void {
+    func hideStatusBarHUD() -> Void {
         
-        stopLoading()
-        
-        generateView()
-        factoryTimer = NSTimer.scheduledTimerWithTimeInterval(0.8, target: self, selector: "generateView", userInfo: nil, repeats: true)
-    }
-    
-    func stopLoading() -> Void {
-        
-        var timer: NSTimer? = factoryTimer
-        
-        self.factoryTimer?.invalidate()
-        self.factoryTimer = nil
-    }
-    
-    func generateView() {
-        
-        var loadingView: UIView = UIView(frame: CGRectMake(CGRectGetWidth(UIScreen.mainScreen().bounds), -20.0,
-            CGRectGetWidth(UIScreen.mainScreen().bounds) / 4.0, 2.0))
-        loadingView.backgroundColor = loaderForegroundColor
-        self.navigationController?.navigationBar.addSubview(loadingView)
-        self.navigationController?.navigationBar.bringSubviewToFront(loadingView)
-        
-        var animation: CABasicAnimation = CABasicAnimation()
-        animation.duration = 2.0
-        animation.keyPath = "position.x";
-        animation.fromValue = CGRectGetWidth(UIScreen.mainScreen().bounds) + CGRectGetWidth(loadingView.frame)
-        animation.toValue = -CGRectGetWidth(loadingView.frame)
-        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        loadingView.layer.addAnimation(animation, forKey: "basic")
+        LoaderHUD.sharedInstance.stopLoadingAnimation(true)
     }
 }
--- HLStatusBarLoaderExample/HLStatusBarLoaderExample/ListViewControllerViewController.swift
+++ HLStatusBarLoaderExample/HLStatusBarLoaderExample/ListViewControllerViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SampleViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,19 +21,17 @@ class SampleViewController: UIViewController, UITableViewDataSource, UITableView
         self.refreshControl = UIRefreshControl()
         self.refreshControl.addTarget(self, action: "refreshControlTriggered:", forControlEvents: UIControlEvents.ValueChanged)
         self.tableView.addSubview(refreshControl)
-        
-        self.loaderForegroundColor = UIColor(red: 205.0/255.0, green: 200.0/255.0, blue: 41.0/255.0, alpha: 0.85)
     }
     
-    func refreshControlTriggered(sender: AnyObject) {
-        
-        self.startLoading()
+    func refreshControlTriggered(sender: AnyObject?) {
+
+        self.showStatusBarHUD()
         
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(4.5 * Double(NSEC_PER_SEC))),
             dispatch_get_main_queue()) { () -> Void in
             
                 self.refreshControl.endRefreshing()
-                self.stopLoading()
+                self.hideStatusBarHUD()
         }
     }
     
@@ -42,6 +40,25 @@ class SampleViewController: UIViewController, UITableViewDataSource, UITableView
         super.viewWillAppear(animated)
         
         self.navigationItem.title = "News Feed"
+        
+        self.statusBarHUDForegroundColor = UIColor(red: 205.0/255.0, green: 200.0/255.0, blue: 41.0/255.0, alpha: 0.85)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        struct TokenHolder {
+            static var token: dispatch_once_t = 0;
+        }
+        
+        dispatch_once(&TokenHolder.token) {
+            
+            self.refreshControl.beginRefreshing()
+            self.tableView.contentOffset = CGPointMake(0.0, self.tableView.contentOffset.y - self.refreshControl.frame.size.height)
+            
+            self.refreshControlTriggered(nil)
+        }
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -58,7 +75,7 @@ class SampleViewController: UIViewController, UITableViewDataSource, UITableView
         
         var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("newsCell") as UITableViewCell
         
-        cell.textLabel?.text = "Hello, world!"
+        cell.textLabel.text = "Hello, world!"
         cell.detailTextLabel?.text = "This is a sample status"
         
         return cell
